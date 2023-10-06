3524a496e4dcd6ff789455a04f65964b1c81c105
B676649 <Bendik_bb@hotmail.com>
2023-10-06 18:07:20 +0200

    Update KortUtils.java

--- src/no/hvl/dat100/prosjekt/modell/KortUtils.java
+++ src/no/hvl/dat100/prosjekt/modell/KortUtils.java
@@ -6,21 +6,25 @@ import no.hvl.dat100.prosjekt.TODO;
 
 public class KortUtils {
 
-	/**
-	 * Sorterer en samling. Rekkefølgen er bestemt av compareTo() i Kort-klassen.
-	 * 
-	 * @see Kort
-	 * 
-	 * @param samling
-	 * 			samling av kort som skal sorteres. 
-	 */
-	
-	public static void sorter(KortSamling samling) {
+
+	static void sorter(KortSamling samling) {
+		Kort[] kortArray = samling.getSamling();
+		int length = samling.getAntalKort();
 		
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
-		// TODO - END
+		for (int i = 0; i < length - 1; i++) {
+			for (int j = 0; j < length - i; j++) {
+				Kort erKort = kortArray[j + 1];
+				
+				if (erKort == null)  {
+					continue;
+				}
+				if( kortArray[j].compareTo(kortArray[j + 1]) > 0) {
+					Kort temp = kortArray[j];
+					kortArray[j] = kortArray[j +1];
+					kortArray[j +1] = temp;
+				}
+			}
+		}
 	}
 	
 	/**
