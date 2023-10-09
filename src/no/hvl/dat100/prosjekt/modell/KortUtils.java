4028cffbeb8220d496abd3073d3281db8a64816c
Simen Strømsnes <676624@stud.hvl.no>
2023-10-09 18:02:00 +0200

    La til ønskede endringer på Utils klassen

--- src/no/hvl/dat100/prosjekt/modell/KortUtils.java
+++ src/no/hvl/dat100/prosjekt/modell/KortUtils.java
@@ -1,42 +1,43 @@
 package no.hvl.dat100.prosjekt.modell;
 
-import java.util.Collections;
-import java.util.Random;
-
-import no.hvl.dat100.prosjekt.TODO;
+//import java.util.Collections;
+//import java.util.Random;
 
 public class KortUtils {
-
-
-	public static void sorter(KortSamling samling) {
-		Kort[] kortarray = samling.getSamling();
-		int length = samling.getAntalKort();
+	public static void sorter(KortSamling kortSamling) {
+		if (kortSamling.erTom()) return;
 		
-		for (int i = 0; i < length - 1; i++) {
-			for (int j = 0; j < length - i; j++) {
-				Kort erkort = kortarray[j + 1];
+		Kort[] samling = kortSamling.getSamling();
+		int antallKort = kortSamling.getAntalKort();
+		
+		for (int i = 0; i < antallKort - 1; i++) {
+			for (int j = 0; j < antallKort - i; j++) {
+				Kort erkort = samling[j+1];
 				
 				if (erkort == null)  {
 					continue;
 				}
-				if( kortarray[j].compareTo(kortarray[j + 1]) > 0) {
-					Kort temp = kortarray[j];
-					kortarray[j] = kortarray[j +1];
-					kortarray[j +1] = temp;
+				
+				if (samling[j].compareTo(samling[j+1]) > 0) {
+					Kort temp = samling[j];
+					samling[j] = samling[j+1];
+					samling[j+1] = temp;
 				}
 			}
 		}
+		return;
 	}
 	
-	public static void stokk(KortSamling samling) {
-		Kort[] kortarray = samling.getSamling();
-		int length = samling.getAntalKort();
+	public static void stokk(KortSamling kortSamling) {
+		Kort[] samling = kortSamling.getSamling();
+		int antallKort = kortSamling.getAntalKort();
 		
-			for (int i = length - 1; i > 0; i--) {				// stokker fra høyest index til start index
-				int randomise = (int)(Math.random()*(i + 1));	// (i + 1) (i > 0 i loopen) er grensene til den randomiserte indexen
-				Kort temp = kortarray[i];						// Math.random er selve randomiseren ofc
-				kortarray[i] = kortarray[randomise];
-				kortarray[randomise] = temp;
-			}	
+		for (int i = antallKort - 1; i >= 0; i--) {							// stokker fra høyest index til start index
+			int tilfeldigIndeks = (int)(Math.random() * antallKort - 1);	// (i + 1) (i >= 0 i loopen) er grensene til den randomiserte indexen
+			Kort temp = samling[i];											// Math.random er selve randomiseren ofc
+			samling[i] = samling[tilfeldigIndeks];
+			samling[tilfeldigIndeks] = temp;
 		}
+		return;
 	}
+}
